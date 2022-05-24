8ae26161f9b4762ca33bf6aadd033481205be43a
Brandonnrg <bradonroldan245@gmail.com>
2022-05-24 00:24:42 -0500

    html contenedores

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/contenedores', function(req, res, next) {
+  res.render('contenedores', { title: 'Contenedores' });
+});
+
 module.exports = router;
