11d552c9566897a5d782756547e7f3a354d80d90
amauri.pineda <amauri.pineda@netlogistik.com>
2022-05-25 22:34:41 -0500

    limpieza de prueba

--- routes/smartphones.js
+++ routes/smartphones.js
@@ -5,12 +5,6 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  const pruebaSmartphone = Smartphone({
-    id: mongoose.Types.ObjectId(),
-    marca: 'Huawei',
-    modelo: 'P20 lite'
-  });
-  console.log('el smartphone', pruebaSmartphone);
   res.send('respond with a resource');
 });
 
